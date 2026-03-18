2d2ea1967dee931725e2bc89fd175fcaf67240fd
Veeranjaneya45 <pp0238@srmist.edu.in>
2026-03-18 14:50:18 +0530

    UC8

---
+++ App/src/UC8.java
@@ -0,0 +1,86 @@
+/**
+ * Author: Veeranjaneya
+ * Use Case 8: Booking History & Reporting
+ */
+
+import java.util.*;
+
+/**
+ * CLASS - Reservation
+ * Represents a confirmed booking
+ */
+class Reservation {
+    private String guestName;
+    private String roomType;
+
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+}
+
+/**
+ * CLASS - BookingHistory
+ * Stores confirmed reservations in order
+ */
+class BookingHistory {
+    private List<Reservation> confirmedReservations;
+
+    public BookingHistory() {
+        confirmedReservations = new ArrayList<>();
+    }
+
+    public void addReservation(Reservation reservation) {
+        confirmedReservations.add(reservation);
+    }
+
+    public List<Reservation> getConfirmedReservations() {
+        return confirmedReservations;
+    }
+}
+
+/**
+ * CLASS - BookingReportService
+ * Generates report from booking history
+ */
+class BookingReportService {
+
+    public void generateReport(BookingHistory history) {
+        System.out.println("Booking History Report");
+
+        for (Reservation r : history.getConfirmedReservations()) {
+            System.out.println("Guest: " + r.getGuestName() +
+                    ", Room Type: " + r.getRoomType());
+        }
+    }
+}
+
+/**
+ * MAIN CLASS
+ */
+public class UC8{
+
+    public static void main(String[] args) {
+
+        BookingHistory history = new BookingHistory();
+
+        // Add confirmed bookings
+        history.addReservation(new Reservation("Abhi", "Single"));
+        history.addReservation(new Reservation("Subha", "Double"));
+        history.addReservation(new Reservation("Vanmathi", "Suite"));
+
+        // Generate report
+        BookingReportService reportService = new BookingReportService();
+
+        System.out.println("Booking History and Reporting\n");
+        reportService.generateReport(history);
+    }
+}
